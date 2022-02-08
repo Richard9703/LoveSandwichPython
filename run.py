98fbf8efefb93893c173c309ffb09b728a1846cf
Richard9703 <richardpaulov@gmail.com>
2022-02-08 10:57:22 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -16,9 +12,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    Get sales figures input from user
+    Get sales figures input from user.
+	Run a while loop to collect a valid string of data user
+	via the terminal, which must be a string of 6 numbers separated
+	by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -35,6 +35,7 @@ def get_sales_data():
 
     return sales_data
 
+
 def validate_data(values):
     """
     Inside the try, converts all string values into intergers,
@@ -42,8 +43,8 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
-        [int(value) for value in values]
-        if len(values) != 6:
+    	[int(value) for value in values]
+    	if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
@@ -53,4 +54,17 @@ def validate_data(values):
 
     return True    
 
+
+def update_sales_worksheet(data):
+	"""
+	Update sales worksheet, add new row with the list data provided
+	"""
+	print("Updating sales worksheet...\n")
+	sales_worksheet = SHEET.worksheet("sales")
+	sales_worksheet.append_row(data)
+	print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()    
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
